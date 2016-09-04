76cff14e5ac2cff76da95890aa08ef127eab0f0d
chepakrul <chepakrulazmi@gmail.com>
2016-09-04 16:02:11 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -18,11 +18,9 @@ zipfile <- "./data/household_power_consumption.zip"
 download.file(fileUrl, destfile, method = "curl")
 unzip(zipfile,overwrite = T, exdir = "./data")
 
-
 # read data into R
 files <- file("./household_power_consumption.txt")
 data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
-
 
 #3- Create/Plot Graph Plot 2
 
