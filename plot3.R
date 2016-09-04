4f89b8406326b758b7854b13bb698df92876378c
chepakrul <chepakrulazmi@gmail.com>
2016-09-04 16:00:28 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -22,7 +22,7 @@ unzip(zipfile,overwrite = T, exdir = "./data")
 # read data into R
 files <- file('./household_power_consumption.txt')
 data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
-dim(data)
+
 
 #3- Create/Plot Graph Plot 3
 
